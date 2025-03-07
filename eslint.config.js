4ce143743af33c37138bd432ddf5f5b39f83d19b
Abhishek Bauri <abhishekbauri12@gmail.com>
2025-03-07 20:51:37 +0530

    setting up routes and layouts

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import unusedImports from 'eslint-plugin-unused-imports' 
 
 export default [
   { ignores: ['dist'] },
@@ -19,6 +20,7 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'unused-imports': unusedImports, 
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -28,6 +30,7 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'unused-imports/no-unused-imports': 'error', 
     },
   },
 ]
