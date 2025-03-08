7102ef0936393d2c8d741c7edba457dd44dcec0a
Abhishek Bauri <abhishekbauri12@gmail.com>
2025-03-08 17:44:35 +0530

    completed landing page

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -14,12 +14,13 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline:
-          "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground hover:cursor-pointer",
+          "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground hover:cursor-pointer rounded-full",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-neutral-300 hover:text-accent-foreground hover:cursor-pointer",
         link: "text-primary underline-offset-4 hover:underline",
+        rose: "bg-rose-500 text-white shadow-md hover:bg-rose-600 px-4 py-2 rounded-full transition duration-200 ease-in-out  ",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
