60a83195fbcee6dfb508bd99562094fd458f1d78
Abhishek Bauri <abhishekbauri12@gmail.com>
2025-03-08 13:06:56 +0530

    completed navbar design

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <header className="w-full bg-white shadow-md p-2 md:p-4 flex justify-between items-center">
+      <div className="flex items-center space-x-2">
+        <img src="/logo.svg" alt="logo" className="w-30 h-8 md:w-35 md:h-10" />
+      </div>
+
+      <nav className="hidden md:flex space-x-4">
+        <Button variant="outline" className=" font-mono">
+          <Plus />
+          Create Tour
+        </Button>
+        <Button variant="ghost" className=" font-inter">
+          Sign In
+        </Button>
+      </nav>
+    </header>
   );
 };
 
